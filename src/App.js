e0da18751fb3e2a62828271218fbe33e7e3d3c19
Abdallah Gamal <58944474+abdallahgamal5500@users.noreply.github.com>
2024-02-08 00:11:57 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ const App = () => {
           .trim()
           .toLowerCase();
         console.log(transcript);
-        if (transcript.includes("zamzam")) {
+        if (transcript.includes("c3")) {
           toggleRecording(); // Start the main speech recognition process
           console.log("************** Start the main speech recognition process");
         } else {
